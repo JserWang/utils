14f6e7c8d03f584c3eae74afafd8a1d1441e213e
JserWang <jserwang@gmail.com>
2021-05-09 22:49:17 +0800

    feat: isFunction

--- src/is.ts
+++ src/is.ts
@@ -1,3 +1,5 @@
 export const isString = (val: any): val is string => typeof val === 'string'
 
 export const isBrowser = (): boolean => typeof window !== 'undefined'
+
+export const isFunction = <T extends Function>(val: any): val is T => typeof val === 'function'
