d0bbb353be5e7b67828870ed21efe70ecd2e0b6d
JserWang <jserwang@gmail.com>
2022-07-02 15:04:59 +0800

    refactor(object): deepMerge

--- src/object/deepMerge.ts
+++ src/object/deepMerge.ts
@@ -1,9 +1,47 @@
 import { isObject } from './isObject'
 
-export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
-  let key: string
-  for (key in target)
-    src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key])
+type MergeInsertions<T> =
+  T extends object
+    ? { [K in keyof T]: MergeInsertions<T[K]> }
+    : T
 
-  return src
+type DeepMerge<F, S> = MergeInsertions<{
+  [K in keyof F | keyof S]: K extends keyof S & keyof F
+    ? DeepMerge<F[K], S[K]>
+    : K extends keyof S
+      ? S[K]
+      : K extends keyof F
+        ? F[K]
+        : never;
+}>
+
+/**
+ * 深拷贝
+ *
+ * @param target
+ * @param sources
+ */
+export function deepMerge<T extends object = object, S extends object = T>(target: T, ...sources: S[]): DeepMerge<T, S> {
+  if (!sources.length) {
+    // @ts-expect-error
+    return target
+  }
+  const source = sources.shift()
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        // @ts-expect-error
+        if (!target[key])
+          Object.assign(target, { [key]: {} })
+        // @ts-expect-error
+        deepMerge(target[key], source[key])
+      }
+      else {
+        Object.assign(target, { [key]: source[key] })
+      }
+    }
+  }
+
+  return deepMerge(target, ...sources)
 }
