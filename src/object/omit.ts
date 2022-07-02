a46cde8a83258198f2fcf3068dd91ed9071b95a6
JserWang <jserwang@gmail.com>
2022-07-02 15:10:38 +0800

    fix(object): omit keys type is string

--- src/object/omit.ts
+++ src/object/omit.ts
@@ -7,8 +7,8 @@ type OmitMultiple<T, K extends Key> = Pick<T, Exclude<keyof T, KeyAsStringType<K
 /***
  * 排除对象中指定fields
  *
+ * @example
  * const obj = { a: 'a', b: 'b', c: 'c' }
- *
  * omit(obj, ['a']) => { b: 'b', c: 'c' }
  *
  * @param val
@@ -18,12 +18,10 @@ export function omit<T extends object, K extends Key>(val: unknown, keys: K): Om
   if (!isObject(val))
     return {} as OmitMultiple<T, K>
 
-  if (typeof keys === 'string')
-    return val[keys] as OmitMultiple<T, K>
-
+  const propertyKeys: string[] = typeof keys === 'string' ? [keys] : keys
   const shallowCopy = { ...val }
 
-  keys.forEach(key => delete shallowCopy[key])
+  propertyKeys.forEach(key => delete shallowCopy[key])
 
   return shallowCopy as OmitMultiple<T, K>
 }
