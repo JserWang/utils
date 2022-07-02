5a68ed4ee25b2218fdbfa783570b03e36cadc497
JserWang <jserwang@gmail.com>
2022-07-02 15:09:59 +0800

    refactor(object): pick

--- src/object/pick.ts
+++ src/object/pick.ts
@@ -3,22 +3,22 @@ import { isObject } from './isObject'
 /**
  * 从对象中取某些key组成新对象
  *
+ * @example
  * const obj = { a: 'a', b: 'b', c: 'c' }
  * pick(obj, ['a', 'b']) => { a: 'a', b: 'b' }
  *
  * @param val
  * @param keys
  */
-export function pick<T extends object, K extends keyof T>(val: unknown, keys: readonly K[]): Pick<T, K> {
+export function pick<T, K extends keyof T>(val: T, keys: K[]): Pick<T, K> {
   if (!isObject(val))
     return {} as Pick<T, K>
 
-  const res = {} as Pick<T, K>
-
-  keys.forEach((key) => {
-    if (key in val)
-      res[key] = val[key]
-  })
-
-  return res
+  return keys.reduce((p, k) => {
+    if (k in val) {
+      if (val[k] !== undefined)
+        p[k] = val[k]
+    }
+    return p
+  }, {} as Pick<T, K>)
 }
