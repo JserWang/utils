da650f62f53f3f4338d92118e1613652b42c92fb
JserWang <jserwang@gmail.com>
2022-07-22 18:00:30 +0800

    fix(object): isEmpty should return true when arg is null、undefined or is not an Object

--- src/object/isEmpty.ts
+++ src/object/isEmpty.ts
@@ -1,15 +1,19 @@
+import { isNullOrUnDef } from '../base/is'
 import { isObject } from './isObject'
 
 /**
  * 对象是否为空
  *
  * @example
- * isEmpty(null) => false
+ * isEmpty(null) => true
  * isEmpty({ a: 1 }) => false
  * isEmpty({}) => true
  *
  * @param val
  */
 export function isEmpty(val: unknown) {
-  return isObject(val) && Object.keys(val).length === 0
+  if (isNullOrUnDef(val) || !isObject(val))
+    return true
+
+  return Object.keys(val).length === 0
 }
--- test/object/isEmpty.test.ts
+++ test/object/isEmpty.test.ts
@@ -2,9 +2,11 @@ import { expect, it } from 'vitest'
 import { isEmpty } from '../../src/object/isEmpty'
 
 it('object isEmpty', () => {
-  expect(isEmpty(null)).toBe(false)
+  expect(isEmpty(null)).toBe(true)
 
-  expect(isEmpty(1)).toBe(false)
+  expect(isEmpty(undefined)).toBe(true)
+
+  expect(isEmpty(1)).toBe(true)
 
   expect(isEmpty({ a: 1 })).toBe(false)
 
