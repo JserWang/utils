ee6a0744e3248541dd8093d97a9cf7326298e976
JserWang <jserwang@gmail.com>
2022-07-22 18:25:52 +0800

    feat(array): unique

---
+++ src/array/unique.ts
@@ -0,0 +1,37 @@
+import type { MaybeArray } from '../base/types'
+import { isNullOrUnDef } from '../base/is'
+import { isArray } from './isArray'
+import { toArray } from './toArray'
+
+/**
+ * 数组去重
+ *
+ * @example
+ *
+ * unique([1, 1, 2, 3, 3]) => [1, 2, 3]
+ *
+ * const arr = [
+ * {
+ *   name: 1
+ * },
+ * {
+ *   name: 2
+ * },
+ * {
+ *   name: 1
+ * }]
+ *
+ * unique(arr, 'name') => [{ name: 1 }, { name: 2 }]
+ *
+ * @param array
+ * @param key
+ */
+export function unique<T>(array: MaybeArray<T>, key?: keyof T) {
+  if (!isArray(array))
+    return toArray(array)
+
+  if (isNullOrUnDef(key))
+    return Array.from(new Set(array))
+
+  return Array.from(new Map(array.map(item => [item[key], item])).values())
+}
---
+++ test/array/unique.test.ts
@@ -0,0 +1,32 @@
+import { expect, it } from 'vitest'
+import { unique } from '../../src/array/unique'
+
+it('array unique', () => {
+  expect(unique(null)).toEqual([])
+
+  expect(unique([1, 1, 2, 3, 3])).toEqual([1, 2, 3])
+
+  expect(unique([], 'name')).toEqual([])
+
+  const expectArr = [
+    {
+      name: 'foo',
+    },
+    {
+      name: 'bar',
+    },
+    {
+      name: 'foo',
+    },
+  ]
+
+  expect(unique(expectArr, 'name'))
+    .toEqual(
+      [{
+        name: 'foo',
+      },
+      {
+        name: 'bar',
+      }],
+    )
+})
