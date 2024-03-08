60ba1a9cb23e2e3f43e2ffcd0120f0e1c0113af1
JserWang <jserwang@gmail.com>
2024-03-08 11:07:14 +0800

    refactor(number): padZero

--- src/number/padZero.ts
+++ src/number/padZero.ts
@@ -8,16 +8,20 @@ import { isNumber } from './isNumber'
  * padZero({}) => ''
  * padZero(null) => ''
  * padZero(undefined) => ''
- * padZero(1, 1) => '01'
- * padZero(1) => '01'
- * padZero('2', 2) => '002'
+ * padZero(1, 1) => '1'
+ * padZero(1, 2) => '01'
+ * padZero('2', 3) => '002'
  *
  * @param val
- * @param count
+ * @param length 期望最终字符串长度
  */
-export function padZero(val: unknown, count = 1) {
-  if (isString(val) || isNumber(val))
-    return `${val}`.padStart(`${val}`.length + count, '0')
+export function padZero(val: unknown, length = 1) {
+  if (isString(val) || isNumber(val)) {
+    if (`${val}`.length >= length)
+      return `${val}`
+
+    return `${val}`.padStart(length, '0')
+  }
 
   return ''
 }
